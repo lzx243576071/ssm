88dbde9d82f596af6abede4186972013f8e509c3
luzhexuan <luzhexuan@yuantutech.com>
2019-08-14 09:00:26 +0800

    漏提交

---
+++ ssm/ssm-master/src/main/java/com/soecode/web/entity/GoodsRecord.java
@@ -0,0 +1,55 @@
+package com.soecode.web.entity;
+
+import java.util.Date;
+
+public class GoodsRecord {
+    private Integer id;
+
+    private Integer orderId;
+
+    private Integer goodsId;
+
+    private Integer goodsState;
+
+    private Date createTime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Integer orderId) {
+        this.orderId = orderId;
+    }
+
+    public Integer getGoodsId() {
+        return goodsId;
+    }
+
+    public void setGoodsId(Integer goodsId) {
+        this.goodsId = goodsId;
+    }
+
+    public Integer getGoodsState() {
+        return goodsState;
+    }
+
+    public void setGoodsState(Integer goodsState) {
+        this.goodsState = goodsState;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+}
---
+++ ssm/ssm-master/src/main/java/com/soecode/web/mapper/GoodsRecordMapper.java
@@ -0,0 +1,18 @@
+package com.soecode.web.mapper;
+
+import com.soecode.web.entity.GoodsRecord;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface GoodsRecordMapper extends Mapper<GoodsRecord> {
+//    int deleteByPrimaryKey(Integer id);
+//
+//    int insert(GoodsRecord record);
+//
+//    int insertSelective(GoodsRecord record);
+//
+//    GoodsRecord selectByPrimaryKey(Integer id);
+//
+//    int updateByPrimaryKeySelective(GoodsRecord record);
+//
+//    int updateByPrimaryKey(GoodsRecord record);
+}
